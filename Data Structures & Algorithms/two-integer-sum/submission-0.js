45f8165926b72621447a658ad184fc2f999c84ac
Ilya Yudin <Yudin2402@gmail.com>
2026-05-12 12:36:42 +0300

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+
+        const map = new Map();
+
+        for(let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+
+            if(map.has(complement)) {
+                return [map.get(complement), i];
+            }
+
+            map.set(nums[i], i);
+        }
+    }
+}
