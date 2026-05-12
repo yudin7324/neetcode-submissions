c55e3acefb9628230ecacfdcfd49ee3bee56f2af
Ilya Yudin <Yudin2402@gmail.com>
2026-05-12 12:09:10 +0300

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        return [...s].sort().join("") === [...t].sort().join("");
+    }
+}
