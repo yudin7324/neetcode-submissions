2ae67c27869852dbb2a3d1e0a16838e951eaf56d
Ilya Yudin <Yudin2402@gmail.com>
2026-05-13 10:44:16 +0300

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const map = {
+            "]": "[",
+            ")": "(",
+            "}": "{",
+        }
+        const stack = [];
+
+        for(const char of s) {
+            if(char === "(" || char === "{" || char === "[") {
+                stack.push(char);
+            } else {
+                if(stack.pop() !== map[char]) {
+                    return false
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
