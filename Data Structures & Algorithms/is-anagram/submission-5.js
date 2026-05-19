664a32765895174a24089e42915efe9c84903dab
Ilya Yudin <Yudin2402@gmail.com>
2026-05-19 09:02:20 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+
+        const map = {}
+
+        for(const char of s) {
+            map[char] = (map[char] || 0) + 1;
+        }
+
+        for(const char of t) {
+            if(!map[char]) {
+                return false;
+            } else {
+                map[char] = map[char] - 1;
+            } 
+        }
+
+        return true;
+    }
+}
